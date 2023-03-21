7e4da1e00939ae76d2835405a98f6ddef77f5cd9
Fawas6 <adeoyemuhdfawas@gmail.com>
2023-03-21 21:25:35 +0100

    added: gradient screen and logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import NotificationsIcon from '@mui/icons-material/NotificationsOutlined';
-import { textAlign } from '@mui/system';
+import { fontSize, textAlign } from '@mui/system';
 
 export default function Home() {
   return (
@@ -10,12 +10,21 @@ export default function Home() {
         <div>
             <div className='page_headertext'>
              <div><h1>Welcome</h1></div>
-             <div className='notify_icon'><h1><NotificationsIcon /></h1></div>
+             <div className='notify_icon'><h1><NotificationsIcon sx={{ fontSize: '30px' }} /></h1></div>
             </div>
             <div className='page_description'>Your current sales auction and activity.</div>
             <hr/>
         </div>
       </header>
+
+      <body>
+
+        <section>
+            <div className='gradient_screen'></div>
+            <img className='rayda_logo' src='logo.png' alt='rayda_logo'/>
+        </section>
+
+      </body>
 
     </div>
   )
