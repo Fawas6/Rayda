d0afb8ccc3489adc4691bf93d59b6602023d1d5a
Fawas6 <adeoyemuhdfawas@gmail.com>
2023-03-22 07:49:30 +0100

    added: time, not live, layers auction, accept invite. Fully respnsive on all devices

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import NotificationsIcon from '@mui/icons-material/NotificationsOutlined';
+import CircleIcon from '@mui/icons-material/Circle'
+import ThumbUpAltOutlinedIcon from '@mui/icons-material/ThumbUpAltOutlined'
 import { fontSize, textAlign } from '@mui/system';
 
 export default function Home() {
@@ -19,9 +21,23 @@ export default function Home() {
 
       <body>
 
-        <section>
+        <section className='sec_one'>
             <div className='gradient_screen'></div>
             <img className='rayda_logo' src='logo.png' alt='rayda_logo'/>
+            <div className='time'>
+                <h1>Starts in: 3 days : 2 hours : 3 minutes</h1>
+                <span style={{ color: "#b16208", fontSize: '15px', border: '#fffaeb 1px solid', padding: '5px', paddingLeft: '12px', paddingRight: '12px', background: '#fffaeb', borderRadius: '20px' }}><CircleIcon className='dot' sx={{ fontSize: '8px' }} /><b>Not Live</b></span><span style={{ color: "#646464", marginLeft: "10px", fontWeight: "520" }}>Layers Auction</span>
+            </div>
+            <div className='invite'>
+                <span style={{ color: '#464444', fontSize: '15px', border: '#d4d4d5 1px solid', padding: '10px', borderRadius: '7px', fontWeight: 'bolder' }}><ThumbUpAltOutlinedIcon className='thumb' sx={{ color: '#464444' }} />Accept Invite</span>
+            </div>
+        </section>
+
+        <section className='sec_two'>
+            <div className='sec_two_heading'>
+                <div className='Featured_items'><h3>Featured Items</h3></div>
+                <span style={{ color: '#464444', fontSize: '15px', border: '#d4d4d5 1px solid', padding: '10px', borderRadius: '7px', fontWeight: 'bolder' }}>View Auction</span>
+            </div>
         </section>
 
       </body>
