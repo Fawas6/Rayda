bc1864dcd30267a28a85a872a8bbb3ac653676f4
Fawas6 <adeoyemuhdfawas@gmail.com>
2023-03-25 03:23:01 +0100

    added: css styles to make the item cards look better

--- src/redux/Items.jsx
+++ src/redux/Items.jsx
@@ -1,29 +1,31 @@
-import { fontWeight } from '@mui/system';
 import * as React from 'react'
 import { useGetDataQuery } from "../redux/apiSlice";
+import { Button } from '@mui/material';
+import { textTransform } from '@mui/system';
 //import { v4 as uuidv4} from 'uuid';
 
 const DataList = () => {
     const { data, error, isLoading, isFetching, isSuccess } = useGetDataQuery();
     
     return (
-        <div className='displayed_items_container'>
-            {isLoading && <div><h4>Loading...</h4></div>}
-            {isFetching && <div><h4>Fetching...</h4></div>}
-            {error && <div><h4>Something went wrong!!!</h4></div>}
-            {isSuccess && 
-                <div>
-                    {data.data.map((item) => (
-                            <div className='displayed_items' key={item.title}>
-                                <img className='item_image' src={item.image} alt="" />
-                                <div className='item_name'><span className='KO'>KO</span> {item.name}<span className='highest_bidder'> (Highest Bidder)</span></div>
-                                <div className='item_title'>{item.title}</div>
-                                <div className='item_bid'><span style={{ fontWeight: '100', color: '#464444' }}>Current Bid:</span> {item.bid}</div>
-                                <hr/>     
-                            </div>
-                    ))}
-                </div>}
-        </div>
+            <div className='displayed_items_container'>
+                {isLoading && <div><h4>Loading...</h4></div>}
+                {isFetching && <div><h4>Fetching...</h4></div>}
+                {error && <div><h4>Something went wrong!!!</h4></div>}
+                {isSuccess && 
+                    <div>
+                        {data.data.map((item) => (
+                                <div className='displayed_items' key={item.title}>
+                                    <img className='item_image' src={item.image} alt="" />
+                                    <div className='item_name'><span className='KO'>KO</span> {item.name}<span className='highest_bidder'> (Highest Bidder)</span></div>
+                                    <div className='item_title'>{item.title}</div>
+                                    <div className='item_bid'><span style={{ fontWeight: '100', color: '#464444' }}>Current Bid:</span> {item.bid}</div>
+                                    <hr/>  
+                                    <div><Button sx={{ marginBottom: '20px', marginTop: '5px', width: '100%', fontWeight: 'bold', fontSize: '13px', textTransform: 'none' }} variant="contained">Add to wishlist</Button></div>
+                                </div>
+                        ))}
+                    </div>}
+            </div>
     )
 }
 
